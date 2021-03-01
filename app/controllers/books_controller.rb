c09f365ecd73f7d41f417ab862b1d8fc49f0e1df
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-03-01 09:10:43 +0000

    [Finish]エラー箇所を修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @books = Book.all
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
@@ -23,13 +24,14 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+
   end
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
       render :edit
     end
